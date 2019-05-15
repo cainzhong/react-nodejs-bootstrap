343b6c802d583fe9d2a0f3c13f0fc8c27150f1a9
Zhong Tao <tzhong@microfocus.com>
2019-05-15 16:59:20 +0800

    historyApiFallback: true, https://webpack.js.org/configuration/dev-server#devserverhot

--- webpack.config.js
+++ webpack.config.js
@@ -5,45 +5,46 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const outputDirectory = 'dist';
 
 module.exports = {
-    entry: ['babel-polyfill', './src/client/index.js'],
-    output: {
-        path: path.join(__dirname, outputDirectory),
-        filename: 'bundle.js'
+  entry: ['babel-polyfill', './src/client/index.js'],
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
     },
-    module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader'
-            }
-        },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                loader: 'url-loader?limit=100000'
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    devServer: {
-        disableHostCheck: true,
-        port: 3000,
-        open: true,
-        proxy: {
-            '/api': 'http://localhost:8080'
-        }
-    },
-    plugins: [
-        new CleanWebpackPlugin([outputDirectory]),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            favicon: './public/favicon.ico'
-        })
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
     ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    disableHostCheck: true,
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
+    })
+  ]
 };
